5794c68d300d7b5bc6e27a0de9505925f23a84e2
Nazik <nazar.gavronsky.mail@gmail.com>
2020-04-07 14:26:12 +0300

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/core/interfaces';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  todoList: Array<Todo>
+  
+  constructor() {
+    this.todoList = todos
+   }
+
+  ngOnInit(): void {
+  }
+
+}
+
+let todos = [
+  {
+    id:1,
+    title: `Angular`,
+    description: ``,
+    isDone: false,
+  },
+  {
+    id:2,
+    title: `JS`,
+    description: `Test`,
+    isDone: true,
+  }
+]
